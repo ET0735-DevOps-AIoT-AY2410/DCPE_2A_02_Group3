ab360dc201ca9c023c0ae0f1f3b0064eb183afa6
Paven <paven.23@ichat.sp.edu.sg>
2024-06-18 18:30:45 +0800

    feat: allowing lcd to display dummy variables

--- src/lcd.py
+++ src/lcd.py
@@ -1,5 +1,17 @@
 from hal import hal_lcd as LCD
+import main as main
 
 
-def lcd_function():
-    lcd = LCD.lcd()
+lcd = LCD.lcd()
+lcd.lcd_clear()
+
+
+def display_item_details():
+    #assign dummy values to functions
+    item_name = "bobby"
+    item_cost = 1.50
+    total_cost = 10.50
+
+    #Display variables on lcd
+    lcd.lcd_display_string(item_name, " || ", item_cost , 1)
+    lcd.lcd_display_string(total_cost, 2)
