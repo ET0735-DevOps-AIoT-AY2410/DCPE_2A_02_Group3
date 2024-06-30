85b95085359f6b6cda148f369b44efdf523052fc
jer123se12 <jerickseng06@gmail.com>
2024-07-01 01:23:31 +0800

    completed creation of db and finished endpoints for products to be read and updated

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -4,12 +4,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 app=Flask(__name__)
-db=mysql.connector.connect(
-	host="localhost",
-	user=os.getenv("DBUSER"),
-	password=os.getenv("DBPASSWORD")
-    database="supermarket"
-)
-DB=db.cursor()
+def getdb():
+    db=mysql.connector.connect(
+        host="localhost",
+        user=os.getenv("DBUSER"),
+        password=os.getenv("DBPASSWORD"),
+        database="supermarket"
+    )
+    return db
 
 
--- server/routes.py
+++ server/routes.py
@@ -1,20 +1,53 @@
 from app import app
-from flask import request
+from app import getdb
+from flask import jsonify,request
 
 
 # get entire list of products database
 # GET /products
 @app.route("/products", methods=["GET"])
 def getProducts():
-    pass
+    conn=getdb()
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM products")
+    results=cur.fetchall()
+    jsons=[]
+    for i in results:
+        jsons.append(
+                {
+                    "id":i[0],
+                    "name":i[1],
+                    "quantity":i[2]
+                    })
+    cur.close()
+    conn.close()
+    return jsons
 
 # edit fields of product database
+# requires at least id
 # PUT /products?{field to edit}={new value}
 @app.route("/products", methods=["PUT"])
 def editProducts():
-    pass
-
-
+    conn=getdb()
+    cur=conn.cursor()
+    if request.args.get("id")==None:
+        return "please specify id", "400"
+    Id=request.args.get('id')
+    name=request.args.get("Name")
+    amt=request.args.get("Quantity")
+    name=("Name = "+name+' ') if not name == None else ""
+    amt=("Quantity = "+amt) if not amt == None else ""
+    if (name =='' and amt ==''):
+        return "Please include changes",400
+    if (name !='' and amt !=''):
+        name+=", "
+    print(f'UPDATE products set {name}{amt} where id = {Id} ')
+    cur.execute(f'UPDATE products SET {name}{amt} where Id = {Id} ')
+    conn.commit()
+    cur.close()
+    conn.close()
+    return 'Update Success', 200
+    
 
 # create new order
 @app.route('/orders', methods=["POST"])
