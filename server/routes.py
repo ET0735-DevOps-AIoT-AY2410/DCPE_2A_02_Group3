3b8d64bf6dc3cb42a63f8fc56eaec06fc3fc2301
jer123se12 <jerickseng06@gmail.com>
2024-07-01 11:04:34 +0800

    added gitignore and creating products

--- server/routes.py
+++ server/routes.py
@@ -23,6 +23,40 @@ def getProducts():
     conn.close()
     return jsons
 
+# create products given name and amount
+@app.route("/newproduct", methods=["POST"])
+def createProduct():
+    conn=getdb()
+    cur=conn.cursor()
+    items = ["Name","Quantity"]
+    fields= ["Name", "Quantity"] 
+    results=[]
+    count=0
+    for i in items:
+        item=request.args.get(i)
+        if item ==None:
+            return (item + "mising from request"), 400
+        results.append("'"+str(item)+"'")
+        count+=1
+    final='INSERT INTO products('+', '.join(fields)+') VALUES('+','.join(results)+')'
+    cur.execute(final)
+    conn.commit()
+    cur.execute("SELECT LAST_INSERT_ID()")
+    res=cur.fetchone()
+    cur.execute("SELECT * FROM products WHERE Id = "+str(res[0]))
+    res=cur.fetchone()
+    finalres={
+            "id":res[0]
+            }
+    for i in range(1, len(res)):
+        finalres[fields[i-1]]=res[i]
+    cur.close()
+    conn.close()
+    return finalres,200
+
+    
+
+
 # edit fields of product database
 # requires at least id
 # PUT /products?{field to edit}={new value}
@@ -35,7 +69,7 @@ def editProducts():
     Id=request.args.get('id')
     name=request.args.get("Name")
     amt=request.args.get("Quantity")
-    name=("Name = "+name+' ') if not name == None else ""
+    name=("Name = '"+name+"' ") if not name == None else ""
     amt=("Quantity = "+amt) if not amt == None else ""
     if (name =='' and amt ==''):
         return "Please include changes",400
