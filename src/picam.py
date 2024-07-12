df09e7c804da8afdfbfde5ab38abd47761bdb860
Paven <paven.23@ichat.sp.edu.sg>
2024-07-12 21:54:38 +0800

    changing from recording to taking picture for picam

--- src/picam.py
+++ src/picam.py
@@ -1,61 +1,31 @@
 from picamera2.encoders import H264Encoder
 from picamera2 import Picamera2, Preview
 import time
-import cv2 
+from PIL import Image
 from pyzbar.pyzbar import decode
+import os
 
-
-def barcode_scanner ():
-    
-    # initialise PiCam
+def capture_image():
     picam2 = Picamera2()
-
-    # initialise video config with 1080p and preview with 480p (preview is live monitor display of picam)
-    video_config = picam2.create_video_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
-    picam2.configure(video_config)
-
-    # setup encoder to allow for video to be encoded
-    encoder = H264Encoder(bitrate=10000000)
-    output = "test.h264"
-
-    # start preview and recording
+    camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)},
+    lores={"size": (640, 480)}, display="lores")
+    picam2.configure(camera_config)
     picam2.start_preview(Preview.QTGL)
-    picam2.start_recording(encoder, output)
+    picam2.start()
+    time.sleep(2)
+    picam2.capture_file("barcode.jpg")
 
-    # use while loop to continously record
-    while True:
-        #capture frame
-        frame = picam2.capture_array()
-        #convert frame to grayscale
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        
-        #decode information of extracted barcodes
-        detected_barcodes = decode(gray)
+def decode_barcode(file):
+    image = Image.open(file)
+    scanned_barcode = decode(image)
+    
+    for barcode in scanned_barcode:
+        barcode_data = barcode.data.decode()
+        print("barcode:", barcode_data)
+    return barcode_data
 
-        #loop through detected barcodes
-        for barcode in detected_barcodes: 
-
-            #locate barcode in image
-            (x,y,w,h) = barcode.rect
-
-            #put rectangle surrounding barcode 
-            cv2.rectangle(frame, ( x + w, y + h), (0, 255, 0), 2)
-                          
-            #print data and type of scanned barcode
-            print("barcode type", barcode.type)
-            print(" barcode data", barcode.data)
-
-            #display image of barcode
-            cv2.imshow("Barcode", frame)
-
-        # Exit loop if key is pressed
-        key_pressed = cv2.waitKey(1) 
-        if key_pressed == ord("q"): 
-            break
-             
-    #stop recording and preview
-    picam2.stop_recording()
-    picam2.stop_preview()
 
 if __name__ == "__main__":
-    barcode_scanner()
+    fn = os.path.basename("barcode.jpg")
+    capture_image()
+    decode_barcode(fn)
