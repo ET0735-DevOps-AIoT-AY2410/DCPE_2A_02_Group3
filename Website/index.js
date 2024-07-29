b9915549554ed6f75fa895624d09a64758e23cdc
jer123se12 <jerickseng06@gmail.com>
2024-07-30 00:57:49 +0800

    finished product page

--- Website/index.js
+++ Website/index.js
@@ -4,7 +4,7 @@ import Api from "./api.js";
 let api = new Api("https://supermarket-backend-xvd6lpv32a-uc.a.run.app/");
 
 // Function to display items on the page
-let cart=[];
+
 let allProducts={};
 // Function to filter and display items based on search query
 function searchItems() {
@@ -14,57 +14,6 @@ function searchItems() {
 }
 
 
-function updateCart(){
-    console.log(cart)
-    setCookie("cart",JSON.stringify(cart),365);
-}
-
-// Function to add an item to the cart
-function addToCart(itemid) {
-    for (let id = 0; id<cart.length; id++){
-        if (cart[id]["itemId"]==itemid){
-            cart[id]["quantity"]+=1
-            updateCart()
-            return
-        }
-    }
-    for (let id = 0; id<allProducts.length; id++){
-        if (allProducts[id]["id"]==itemid){
-            cart.push({"itemId": itemid,"price": allProducts[id]["price"], "quantity":1})
-            updateCart();
-            break
-        }
-    }
-}
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-        }
-}
-return "";
-}
-function configcooke(){
-    cart=getCookie("cart")
-    if (cart==""){
-        setCookie("cart",JSON.stringify([]),365);
-    }
-    cart=JSON.parse(getCookie("cart"))
-    console.log(cart)
-}
 function initalise(){
     displayProducts();
     configcooke();
